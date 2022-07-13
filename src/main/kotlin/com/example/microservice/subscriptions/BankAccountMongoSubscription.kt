471678c81b49a8f3451a33e74d147df479eff37e
Alexander <alexander.bryksin@yandex.ru>
2022-07-13 16:41:05 +0300

    feature: add kafka subscription handle method

--- src/main/kotlin/com/example/microservice/subscriptions/BankAccountMongoSubscription.kt
+++ src/main/kotlin/com/example/microservice/subscriptions/BankAccountMongoSubscription.kt
@@ -2,6 +2,9 @@ package com.example.microservice.subscriptions
 
 import com.example.microservice.lib.es.Event
 import com.example.microservice.lib.es.EventSourcingUtils
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
@@ -14,16 +17,22 @@ class BankAccountMongoSubscription {
 
     companion object {
         private val log = LoggerFactory.getLogger(BankAccountMongoSubscription::class.java)
-        private val SERVICE_NAME = "microservice"
+        private const val handleTimeoutMillis = 5000L
+        private val errorhandler = CoroutineExceptionHandler { _, throwable ->
+            run { log.error("(CoroutineExceptionHandler) handleMessage: {}", throwable.stackTraceToString()) }
+        }
     }
-
 
     @KafkaListener(
         topics = ["\${microservice.kafka.topics.bank-account-event-store}"],
         groupId = "\${microservice.kafka.groupId}",
         concurrency = "\${microservice.kafka.default-concurrency}"
     )
-    fun bankAccountMongoSubscription(@Payload data: ByteArray, ack: Acknowledgment) {
+    fun bankAccountMongoSubscription(@Payload data: ByteArray, ack: Acknowledgment) = runBlocking(errorhandler) {
+        handleMessage(data, ack)
+    }
+
+    private suspend fun handleMessage(data: ByteArray, ack: Acknowledgment) = withTimeout(handleTimeoutMillis) {
         log.info("Subscription data: {}", String(data))
         val deserializedEvents = EventSourcingUtils.deserializeFromJsonBytes(data, Array<Event>::class.java)
         log.info("Subscription deserializedEvents: {}", deserializedEvents)
