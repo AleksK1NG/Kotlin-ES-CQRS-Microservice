d66b3934a5cd1dc29f60bc727c4ce4c37b69f174
Alexander <alexander.bryksin@yandex.ru>
2022-07-13 10:11:50 +0300

    feature: add kafka init

---
+++ src/main/kotlin/com/example/microservice/configuration/KafkaConsumerConfig.kt
@@ -0,0 +1,49 @@
+package com.example.microservice.configuration
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.ContainerProperties
+import org.springframework.kafka.listener.DefaultErrorHandler
+import org.springframework.util.backoff.FixedBackOff
+
+
+@Configuration
+class KafkaConsumerConfig(
+    private val kafkaConfigProperties: KafkaConfigProperties,
+    @Value(value = "\${kafka.bootstrapServers:localhost:9093}")
+    private val bootstrapServers: String
+) {
+
+
+    @Bean
+    fun kafkaListenerContainerFactory(consumerFactory: ConsumerFactory<String, ByteArray>): ConcurrentKafkaListenerContainerFactory<String, ByteArray> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, ByteArray>()
+        factory.consumerFactory = consumerFactory
+        factory.setConcurrency(Runtime.getRuntime().availableProcessors())
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
+        factory.setCommonErrorHandler(DefaultErrorHandler(FixedBackOff(1000L, 2L)))
+        return factory
+    }
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, ByteArray> {
+        return DefaultKafkaConsumerFactory(consumerProps())
+    }
+
+    private fun consumerProps(): Map<String, Any> {
+        val consumerProps: MutableMap<String, Any> = HashMap()
+        consumerProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers!!
+        consumerProps[ConsumerConfig.GROUP_ID_CONFIG] = kafkaConfigProperties!!.orderMongoProjectionGroupId
+        consumerProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        consumerProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = ByteArrayDeserializer::class.java
+        consumerProps[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = kafkaConfigProperties!!.enableAutoCommit
+        return consumerProps
+    }
+}
---
+++ src/main/kotlin/com/example/microservice/configuration/KafkaTopicConfiguration.kt
@@ -0,0 +1,46 @@
+package com.example.microservice.configuration
+
+import com.example.microservice.controllers.BankAccountController
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.clients.admin.NewTopic
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.KafkaAdmin
+
+
+@Configuration
+class KafkaTopicConfiguration(
+    @Value(value = "\${kafka.bootstrapServers:localhost:9093}")
+    private val bootstrapServers: String,
+    @Value(value = "\${microservice.kafka.topics.bank-account-event-store:bank-account-event-store}")
+    private val bankAccountTopicName: String
+) {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(BankAccountController::class.java)
+    }
+
+    @Bean
+    fun kafkaAdmin(): KafkaAdmin {
+        val configs: MutableMap<String, Any> = HashMap()
+        configs[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        return KafkaAdmin(configs)
+    }
+
+    @Bean
+    fun bankAccountEventStoreTopicInitializer(kafkaAdmin: KafkaAdmin): NewTopic? {
+        return try {
+            val topic = NewTopic(bankAccountTopicName, 3, 1.toShort())
+            kafkaAdmin.createOrModifyTopics(topic)
+            log.info("(bankAccountEventStoreTopicInitializer) topic: {}", topic)
+            topic
+        } catch (e: Exception) {
+            log.error(e.message)
+            null
+        }
+    }
+
+
+}
