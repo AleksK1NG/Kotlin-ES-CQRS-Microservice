a873f5004711b394c1da3c30c3a96f37ee97032c
Alexander <alexander.bryksin@yandex.ru>
2022-07-16 17:10:24 +0300

    feature: add tracing init

--- src/main/kotlin/com/example/microservice/domain/BankAccountMongoProjection.kt
+++ src/main/kotlin/com/example/microservice/domain/BankAccountMongoProjection.kt
@@ -9,7 +9,10 @@ import com.example.microservice.lib.es.Serializer
 import com.example.microservice.lib.es.exceptions.UnknownEventTypeException
 import com.example.microservice.repository.BankAccountMongoRepository
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
+import org.springframework.cloud.sleuth.Tracer
+import org.springframework.cloud.sleuth.instrument.kotlin.asContextElement
 import org.springframework.stereotype.Service
 import reactor.util.Loggers
 
@@ -17,37 +20,48 @@ import reactor.util.Loggers
 @Service
 class BankAccountMongoProjection(
     private val mongoRepository: BankAccountMongoRepository,
-    private val serializer: Serializer
+    private val serializer: Serializer,
+    private val tracer: Tracer
 ) : Projection {
     companion object {
         val log = Loggers.getLogger(BankAccountMongoProjection::class.java)
     }
 
-    override suspend fun whenEvent(event: Event) = withTimeout(5000) {
-        log.info("(BankAccountMongoProjection) whenEvent event: $event")
+    override suspend fun whenEvent(event: Event) = withContext(tracer.asContextElement()) {
+        withTimeout(5000) {
+            val span = tracer.nextSpan(tracer.currentSpan()).start().name("BankAccountMongoProjection.whenEvent")
+            log.info("(BankAccountMongoProjection) whenEvent event: $event")
 
-        when (val deserializedEvent = serializer.deserialize(event)) {
-            is BankAccountCreatedEvent -> {
-                val bankAccountDocument = BankAccountDocument(event.aggregateId, deserializedEvent.email, deserializedEvent.balance, deserializedEvent.currency)
-                val savedDocument = mongoRepository.save(bankAccountDocument)
-                log.info("(BankAccountMongoProjection) BankAccountCreatedEvent savedDocument: $savedDocument")
+            try {
+                when (val deserializedEvent = serializer.deserialize(event)) {
+                    is BankAccountCreatedEvent -> {
+                        val bankAccountDocument = BankAccountDocument(event.aggregateId, deserializedEvent.email, deserializedEvent.balance, deserializedEvent.currency)
+                        val savedDocument = mongoRepository.save(bankAccountDocument)
+                        span.tag("savedDocument", savedDocument.toString())
+                        log.info("(BankAccountMongoProjection) BankAccountCreatedEvent savedDocument: $savedDocument")
+                    }
+
+                    is BalanceDepositedEvent -> {
+                        val bankAccountDocument = mongoRepository.findByAggregateId(event.aggregateId).first()
+                        bankAccountDocument.balance = bankAccountDocument.balance?.add(deserializedEvent.balance)
+                        val savedDocument = mongoRepository.save(bankAccountDocument)
+                        span.tag("savedDocument", savedDocument.toString())
+                        log.info("(BankAccountMongoProjection) BalanceDepositedEvent savedDocument: $savedDocument")
+                    }
+
+                    is EmailChangedEvent -> {
+                        val bankAccountDocument = mongoRepository.findByAggregateId(event.aggregateId).first()
+                        bankAccountDocument.email = deserializedEvent.email
+                        val savedDocument = mongoRepository.save(bankAccountDocument)
+                        span.tag("savedDocument", savedDocument.toString())
+                        log.info("(BankAccountMongoProjection) EmailChangedEvent savedDocument: $savedDocument")
+                    }
+
+                    else -> throw UnknownEventTypeException("unknown event: $deserializedEvent")
+                }
+            } finally {
+                span.end()
             }
-
-            is BalanceDepositedEvent -> {
-                val bankAccountDocument = mongoRepository.findByAggregateId(event.aggregateId).first()
-                bankAccountDocument.balance = bankAccountDocument.balance?.add(deserializedEvent.balance)
-                val savedDocument = mongoRepository.save(bankAccountDocument)
-                log.info("(BankAccountMongoProjection) BalanceDepositedEvent savedDocument: $savedDocument")
-            }
-
-            is EmailChangedEvent -> {
-                val bankAccountDocument = mongoRepository.findByAggregateId(event.aggregateId).first()
-                bankAccountDocument.email = deserializedEvent.email
-                val savedDocument = mongoRepository.save(bankAccountDocument)
-                log.info("(BankAccountMongoProjection) EmailChangedEvent savedDocument: $savedDocument")
-            }
-
-            else -> throw UnknownEventTypeException("unknown event: $deserializedEvent")
         }
     }
 }
--- src/main/kotlin/com/example/microservice/queries/BankAccountQueryServiceImpl.kt
+++ src/main/kotlin/com/example/microservice/queries/BankAccountQueryServiceImpl.kt
@@ -8,6 +8,8 @@ import com.example.microservice.repository.BankAccountMongoRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
+import org.springframework.cloud.sleuth.Tracer
+import org.springframework.cloud.sleuth.instrument.kotlin.asContextElement
 import org.springframework.stereotype.Service
 import reactor.util.Loggers
 
@@ -16,29 +18,46 @@ import reactor.util.Loggers
 class BankAccountQueryServiceImpl(
     private val aggregateStore: AggregateStore,
     private val mongoRepository: BankAccountMongoRepository,
+    private val tracer: Tracer
 ) : BankAccountQueryService {
     companion object {
         private val log = Loggers.getLogger(BankAccountQueryServiceImpl::class.java)
     }
 
-    override suspend fun handle(query: GetBankAccountByIdQuery): BankAccountResponse = withContext(Dispatchers.IO) {
-        if (query.fromStore) {
-            return@withContext BankAccountResponse.of(aggregateStore.load(query.aggregateId, BankAccountAggregate::class.java)).also {
-                log.info("(GetBankAccountByIdQuery) fromStore bankAccountResponse: $it")
-            }
-        }
+    override suspend fun handle(query: GetBankAccountByIdQuery): BankAccountResponse = withContext(Dispatchers.IO + tracer.asContextElement()) {
+        val span = tracer.nextSpan(tracer.currentSpan()).start().name("GetBankAccountByIdQuery.handle")
+        span.tag("query span started", "cool =D")
 
         try {
+            if (query.fromStore) {
+                return@withContext BankAccountResponse.of(aggregateStore.load(query.aggregateId, BankAccountAggregate::class.java)).also {
+                    log.info("(GetBankAccountByIdQuery) fromStore bankAccountResponse: $it")
+                    span.tag("BankAccountAggregate", it.toString())
+                }
+            }
+
             val bankAccountDocument = mongoRepository.findByAggregateId(query.aggregateId).first().also {
+                span.tag("bankAccountDocument", it.toString())
                 log.info("(GetBankAccountByIdQuery) LOADED bankAccountDocument: $it")
             }
+
             return@withContext BankAccountResponse.of(bankAccountDocument).also { log.info("(GetBankAccountByIdQuery) bankAccountDocument: $it") }
         } catch (ex: Exception) {
-            val bankAccountAggregate =
-                aggregateStore.load(query.aggregateId, BankAccountAggregate::class.java).also { log.info("(GetBankAccountByIdQuery) bankAccountAggregate: $it") }
-            val savedBankAccountDocument =
-                mongoRepository.save(BankAccountDocument.of(bankAccountAggregate)).also { log.info("(GetBankAccountByIdQuery) savedBankAccountDocument: $it") }
+            span.error(ex)
+
+            val bankAccountAggregate = aggregateStore.load(query.aggregateId, BankAccountAggregate::class.java).also {
+                log.info("(GetBankAccountByIdQuery) bankAccountAggregate: $it")
+                span.tag("bankAccountAggregate", it.toString())
+            }
+
+            val savedBankAccountDocument = mongoRepository.save(BankAccountDocument.of(bankAccountAggregate)).also {
+                log.info("(GetBankAccountByIdQuery) savedBankAccountDocument: $it")
+                span.tag("savedBankAccountDocument", it.toString())
+            }
+
             BankAccountResponse.of(savedBankAccountDocument)
+        } finally {
+            span.end()
         }
     }
 }
