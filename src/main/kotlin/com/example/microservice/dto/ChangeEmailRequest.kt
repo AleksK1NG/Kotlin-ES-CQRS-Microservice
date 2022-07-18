33b27ddb117f026a60161763f37ea98d6a9d5b2a
Alexander <alexander.bryksin@yandex.ru>
2022-07-18 10:17:00 +0300

    feature: add http request validation

--- src/main/kotlin/com/example/microservice/dto/ChangeEmailRequest.kt
+++ src/main/kotlin/com/example/microservice/dto/ChangeEmailRequest.kt
@@ -1,3 +1,6 @@
 package com.example.microservice.dto
 
-data class ChangeEmailRequest(var email: String)
+import javax.validation.constraints.Email
+import javax.validation.constraints.Size
+
+data class ChangeEmailRequest(@get:Email(message = "invalid email") @get:Size(min = 6, max = 60) var email: String)
--- src/main/kotlin/com/example/microservice/dto/DepositBalanceRequest.kt
+++ src/main/kotlin/com/example/microservice/dto/DepositBalanceRequest.kt
@@ -1,5 +1,7 @@
 package com.example.microservice.dto
 
 import java.math.BigDecimal
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.NotNull
 
-data class DepositBalanceRequest(val amount: BigDecimal)
+data class DepositBalanceRequest(@get:DecimalMin(value = "0.00", inclusive = false, message = "invalid amount") @NotNull val amount: BigDecimal)
