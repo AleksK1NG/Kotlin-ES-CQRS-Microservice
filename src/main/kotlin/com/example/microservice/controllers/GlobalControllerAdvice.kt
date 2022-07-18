d948207bd3dfa3dc0c847e43a6f078c813a25b32
Alexander <alexander.bryksin@yandex.ru>
2022-07-18 16:48:00 +0300

    feature: add updates and improvements

--- src/main/kotlin/com/example/microservice/controllers/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/example/microservice/controllers/GlobalControllerAdvice.kt
@@ -7,6 +7,7 @@ import com.example.microservice.exceptions.InvalidEmailException
 import com.example.microservice.lib.es.exceptions.SerializationException
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -29,7 +30,7 @@ class GlobalControllerAdvice {
     @ExceptionHandler(value = [RuntimeException::class])
     fun handleRuntimeException(ex: RuntimeException, request: ServerHttpRequest): ResponseEntity<ErrorHttpResponse> {
         val errorHttpResponse = ErrorHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.message ?: "", LocalDateTime.now().toString())
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorHttpResponse).also {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).contentType(MediaType.APPLICATION_JSON).body(errorHttpResponse).also {
             log.error("(GlobalControllerAdvice) RuntimeException", ex)
         }
     }
@@ -37,27 +38,30 @@ class GlobalControllerAdvice {
     @ExceptionHandler(value = [AggregateNotFountException::class])
     fun handleAggregateNotFoundException(ex: AggregateNotFountException, request: ServerHttpRequest): ResponseEntity<ErrorHttpResponse> {
         val errorHttpResponse = ErrorHttpResponse(HttpStatus.NOT_FOUND.value(), ex.message ?: "", LocalDateTime.now().toString())
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorHttpResponse).also { log.error("(GlobalControllerAdvice) NOT_FOUND", ex) }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON).body(errorHttpResponse)
+            .also { log.error("(GlobalControllerAdvice) AggregateNotFountException NOT_FOUND", ex) }
     }
 
     @ExceptionHandler(value = [SerializationException::class, InvalidEmailException::class, InvalidAmountException::class])
     fun handleBadRequestException(ex: RuntimeException, request: ServerHttpRequest): ResponseEntity<ErrorHttpResponse> {
         val errorHttpResponse = ErrorHttpResponse(HttpStatus.BAD_REQUEST.value(), ex.message ?: "", LocalDateTime.now().toString())
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorHttpResponse).also { log.error("(GlobalControllerAdvice) BAD_REQUEST", ex) }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(errorHttpResponse)
+            .also { log.error("(GlobalControllerAdvice) BAD_REQUEST", ex) }
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = [MethodArgumentNotValidException::class])
-    fun handleInvalidArgument(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, String>>? {
+    fun handleInvalidArgument(ex: MethodArgumentNotValidException): ResponseEntity<MutableMap<String, String>> {
         val errorMap: MutableMap<String, String> = HashMap()
         ex.bindingResult.fieldErrors.forEach { error -> error.defaultMessage?.let { errorMap[error.field] = it } }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(errorMap)
+            .also { log.error("(GlobalControllerAdvice) WebExchangeBindException BAD_REQUEST", ex) }
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = [WebExchangeBindException::class])
-    fun handleWebExchangeInvalidArgument(ex: WebExchangeBindException): ResponseEntity<Map<String, Any?>> {
-        val errorMap: MutableMap<String, Any> = HashMap()
+    fun handleWebExchangeInvalidArgument(ex: WebExchangeBindException): ResponseEntity<MutableMap<String, Any>> {
+        val errorMap = mutableMapOf<String, Any>()
         ex.bindingResult.fieldErrors.forEach { error ->
             error.defaultMessage?.let {
                 errorMap[error.field] = mapOf(
@@ -66,6 +70,7 @@ class GlobalControllerAdvice {
                 )
             }
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(errorMap)
+            .also { log.error("(GlobalControllerAdvice) WebExchangeBindException BAD_REQUEST", ex) }
     }
 }
